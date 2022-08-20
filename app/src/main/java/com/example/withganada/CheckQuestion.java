774a5424f9f32d0ff4783aff32c6f91fd92059c5
SeongeunKim-sonja <batakkse@gmail.com>
2022-08-21 01:11:50 +0900

    Setting Demo Activity

---
+++ app/src/main/java/com/example/withganada/CheckQuestion.java
@@ -0,0 +1,14 @@
+package com.example.withganada;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CheckQuestion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_check_question);
+    }
+}
