3bdb458e3020b18c9d24da3cd67cf1475e133f4f
SeongeunKim-sonja <batakkse@gmail.com>
2022-08-21 15:09:22 +0900

    Demo Activity via Android Studio

--- app/src/main/java/com/example/withganada/Question.java
+++ app/src/main/java/com/example/withganada/Question.java
@@ -2,13 +2,38 @@ package com.example.withganada;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Question extends AppCompatActivity {
+
+    Button CheckAns;
+    TextView StageIndex;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question);
+        Intent intent = getIntent(); /*데이터 수신*/
+        int Stagenum = intent.getExtras().getInt("stageIndex");
+        //어떤 단원의 문제인지 데이터 넘겨받기
+        StageIndex = findViewById(R.id.textView2);
+        StageIndex.setText(Integer.toString(Stagenum) + "단원의 문제를 출력하는 부분");
+
+        CheckAns = findViewById(R.id.btn_check);
+        CheckAns.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //////////////////////////////////////////////
+                //추후 이 부분에 마지막 문제인지 체크하는 부분 넣을 것
+                /////////////////////////////////////////////
+                Intent intent = new Intent(getApplicationContext(), CheckQuestion.class);
+                startActivity(intent);	//메인액티비티로 이동
+                finish();	//문제 액티비티 종료
+            }
+        });
     }
 }
--- app/src/main/java/com/example/withganada/Splash.java
+++ app/src/main/java/com/example/withganada/Splash.java
@@ -2,7 +2,9 @@ package com.example.withganada;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class Splash extends AppCompatActivity {
 
@@ -10,5 +12,21 @@ public class Splash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        MovetoMain(1);
+    }
+
+    private void MovetoMain(int sec) {
+        new Handler().postDelayed(new Runnable()        //thread.sleep시 레이아웃 로딩도 멈춘다.
+        {
+            @Override
+            public void run()
+            {
+                //new Intent(현재 context, 이동할 activity)
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);	//메인액티비티로 이동
+                finish();	//스플래시 액티비티 종료
+            }
+        }, 1000 * sec); // sec초 정도 딜레이를 준 후 시작
     }
 }
