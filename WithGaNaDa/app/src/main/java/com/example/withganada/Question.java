b59fb8ea2a35127e76480297f75f8d304d3342a1
SeongeunKim-sonja <batakkse@gmail.com>
2022-08-23 18:47:59 +0900

    Add ProgressBar

--- WithGaNaDa/app/src/main/java/com/example/withganada/Question.java
+++ WithGaNaDa/app/src/main/java/com/example/withganada/Question.java
@@ -8,20 +8,27 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.kofigyan.stateprogressbar.StateProgressBar;
+
 public class Question extends AppCompatActivity {
 
     Button CheckAns;
     TextView StageIndex;
+    int Currentstate = 1;
+    int Stagenum;
+    StateProgressBar stateProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question);
         Intent intent = getIntent(); /*데이터 수신*/
-        int Stagenum = intent.getExtras().getInt("stageIndex");
+        Stagenum = intent.getExtras().getInt("stageIndex");
         //어떤 단원의 문제인지 데이터 넘겨받기
         StageIndex = findViewById(R.id.textView2);
-        StageIndex.setText(Integer.toString(Stagenum) + "단원의 문제를 출력하는 부분");
+        stateProgressBar = findViewById(R.id.progress_bar);
+        SetProgressView();
+
 
         CheckAns = findViewById(R.id.btn_check);
         CheckAns.setOnClickListener(new View.OnClickListener() {
@@ -29,11 +36,46 @@ public class Question extends AppCompatActivity {
             public void onClick(View view) {
                 //////////////////////////////////////////////
                 //추후 이 부분에 마지막 문제인지 체크하는 부분 넣을 것
+                if (Currentstate >=5){
+                    Intent intent = new Intent(getApplicationContext(), CheckQuestion.class);
+                    startActivity(intent);	//메인액티비티로 이동
+                    finish();	//문제 액티비티 종료
+                }
+                else {
+                    Currentstate++;
+                    SetProgressView();
+                }
                 /////////////////////////////////////////////
-                Intent intent = new Intent(getApplicationContext(), CheckQuestion.class);
-                startActivity(intent);	//메인액티비티로 이동
-                finish();	//문제 액티비티 종료
+
             }
         });
+
+
+    }
+
+    public void SetProgressView(){
+        //현재 단계를 나타내는 부분 처리
+        StageIndex.setText(Integer.toString(Stagenum) + "단원의 문제를 출력하는 부분입니다.\n현재 문항 : " + Integer.toString(Currentstate) + "번");
+        switch (Currentstate){
+
+            case 1:
+                stateProgressBar.setCurrentStateNumber(StateProgressBar.StateNumber.ONE);
+                break;
+            case 2:
+                stateProgressBar.setCurrentStateNumber(StateProgressBar.StateNumber.TWO);
+                break;
+            case 3:
+                stateProgressBar.setCurrentStateNumber(StateProgressBar.StateNumber.THREE);
+                break;
+            case 4:
+                stateProgressBar.setCurrentStateNumber(StateProgressBar.StateNumber.FOUR);
+                break;
+            case 5:
+                stateProgressBar.setAllStatesCompleted(true);
+
+
+
+        }
+
     }
 }
