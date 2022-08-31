50e6826c9a8bfaeb42de79bb981532f8bdeb9a30
SeongeunKim-sonja <batakkse@gmail.com>
2022-08-31 23:59:25 +0900

    Add server transport

--- WithGaNaDa/app/src/main/java/com/example/withganada/Question.java
+++ WithGaNaDa/app/src/main/java/com/example/withganada/Question.java
@@ -3,12 +3,21 @@ package com.example.withganada;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.kofigyan.stateprogressbar.StateProgressBar;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
 public class Question extends AppCompatActivity {
 
@@ -17,6 +26,23 @@ public class Question extends AppCompatActivity {
     int Currentstate = 1;
     int Stagenum;
     StateProgressBar stateProgressBar;
+
+    //통신 헤더 선언부
+    Button send_button;
+    EditText send_editText;
+    TextView send_textView;
+    TextView read_textView;
+    private Socket client;
+    private DataOutputStream dataOutput;
+    private DataInputStream dataInput;
+    private static String SERVER_IP = "192.168.219.100";
+    private static String CONNECT_MSG = "connect";
+    private static String STOP_MSG = "stop";
+
+    private static int BUF_SIZE = 100;
+    //
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +54,20 @@ public class Question extends AppCompatActivity {
         StageIndex = findViewById(R.id.textView2);
         stateProgressBar = findViewById(R.id.progress_bar);
         SetProgressView();
+
+
+        send_button = findViewById(R.id.send_button);
+        send_editText = findViewById(R.id.send_editText);
+        send_textView = findViewById(R.id.send_textView);
+        read_textView = findViewById(R.id.read_textView);
+
+        send_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Connect connect = new Connect();
+                connect.execute(CONNECT_MSG);
+            }
+        });
 
 
         CheckAns = findViewById(R.id.btn_check);
@@ -78,4 +118,55 @@ public class Question extends AppCompatActivity {
         }
 
     }
+
+
+    private class Connect extends AsyncTask< String , String,Void > {
+        private String output_message;
+        private String input_message;
+
+        @Override
+        protected Void doInBackground(String... strings) {
+            try {
+                client = new Socket(SERVER_IP, 12345);
+                dataOutput = new DataOutputStream(client.getOutputStream());
+                dataInput = new DataInputStream(client.getInputStream());
+                output_message = strings[0];
+                dataOutput.writeUTF(output_message);
+
+            } catch (UnknownHostException e) {
+                String str = e.getMessage().toString();
+                Log.w("discnt", str + " 1");
+            } catch (IOException e) {
+                String str = e.getMessage().toString();
+                Log.w("discnt", str + " 2");
+            }
+
+            while (true){
+                try {
+                    byte[] buf = new byte[BUF_SIZE];
+                    int read_Byte  = dataInput.read(buf);
+                    input_message = new String(buf, 0, read_Byte);
+                    if (!input_message.equals(STOP_MSG)){
+                        publishProgress(input_message);
+                    }
+                    else{
+                        break;
+                    }
+                    Thread.sleep(2);
+                } catch (IOException | InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(String... params){
+            send_textView.setText(""); // Clear the chat box
+            send_textView.append("보낸 메세지: " + output_message );
+            read_textView.setText(""); // Clear the chat box
+            read_textView.append("받은 메세지: " + params[0]);
+        }
+    }
+
 }
