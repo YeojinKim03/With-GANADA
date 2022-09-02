b8aff30ce5562a39249722aa0782cd169f8adb9b
SeongeunKim-sonja <batakkse@gmail.com>
2022-09-02 23:01:54 +0900

    Add record module

--- WithGaNaDa/app/src/main/java/com/example/withganada/Question.java
+++ WithGaNaDa/app/src/main/java/com/example/withganada/Question.java
@@ -1,20 +1,32 @@
 package com.example.withganada;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.ContentValues;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.kofigyan.stateprogressbar.StateProgressBar;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.net.Socket;
 import java.net.UnknownHostException;
@@ -35,7 +47,7 @@ public class Question extends AppCompatActivity {
     private Socket client;
     private DataOutputStream dataOutput;
     private DataInputStream dataInput;
-    private static String SERVER_IP = "192.168.219.100";
+    private static String SERVER_IP = "116.41.108.67";
     private static String CONNECT_MSG = "connect";
     private static String STOP_MSG = "stop";
 
@@ -43,6 +55,10 @@ public class Question extends AppCompatActivity {
     //
 
 
+    MediaRecorder recorder;
+    String fileName;
+    MediaPlayer mediaPlayer;
+    int position = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,6 +67,9 @@ public class Question extends AppCompatActivity {
         Intent intent = getIntent(); /*데이터 수신*/
         Stagenum = intent.getExtras().getInt("stageIndex");
         //어떤 단원의 문제인지 데이터 넘겨받기
+
+        permissionCheck();      //마이크 권한 체크
+
         StageIndex = findViewById(R.id.textView2);
         stateProgressBar = findViewById(R.id.progress_bar);
         SetProgressView();
@@ -90,9 +109,106 @@ public class Question extends AppCompatActivity {
             }
         });
 
+        // SD 카드 폴더 지정
+        File file = new File(getApplicationInfo().dataDir, "recorded.mp4");
+        fileName = file.getAbsolutePath();  // 파일 위치 가져옴
+        Toast.makeText(getApplicationContext(), "파일 위치:"+fileName, Toast.LENGTH_SHORT).show();
+
+        findViewById(R.id.test_rec).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {   // 녹음 시작
+                if (recorder == null) {
+                    recorder = new MediaRecorder(); // 미디어리코더 객체 생성
+                }
+                recorder.setAudioSource(MediaRecorder.AudioSource.MIC); // 오디오 입력 지정(마이크)
+                recorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);    // 출력 형식 지정
+                //마이크로 들어오는 음성데이터는 용량이 크기 때문에 압축이 필요
+                recorder.setAudioEncoder(MediaRecorder.AudioEncoder.DEFAULT);   // 인코딩
+                recorder.setOutputFile(fileName);  // 음성 데이터를 저장할 파일 지정
+                try {
+                    recorder.prepare();
+                    recorder.start();
+                    Toast.makeText(getApplicationContext(), "녹음시작", Toast.LENGTH_SHORT).show();
+                } catch (Exception e) {e.printStackTrace();}
+            }
+        });
+
+        findViewById(R.id.test_stop).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {   // 녹음 중지
+                if (recorder != null) {
+                    recorder.stop();
+                    recorder.release();
+                    recorder = null;
+                }
+                Toast.makeText(getApplicationContext(), "녹음중지", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        findViewById(R.id.test_play).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {  // 재생
+                try {
+                    if(mediaPlayer != null){    // 사용하기 전에
+                        mediaPlayer.release();  // 리소스 해제
+                        mediaPlayer = null;
+                    }
+                    mediaPlayer = new MediaPlayer();
+                    mediaPlayer.setDataSource(fileName); // 음악 파일 위치 지정
+                    mediaPlayer.prepare();  // 미리 준비
+                    mediaPlayer.start();    // 재생
+                    Toast.makeText(getApplicationContext(), "재생시작", Toast.LENGTH_SHORT).show();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+
+        findViewById(R.id.test_p_stop).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { // 정지
+                if(mediaPlayer != null && mediaPlayer.isPlaying()) {
+                    mediaPlayer.stop(); // 정지
+                    Toast.makeText(getApplicationContext(), "정지", Toast.LENGTH_SHORT).show();
+                    //save();
+                }
+            }
+        });
 
     }
 
+    // 녹음한 파일 저장
+    public Uri save(){
+        ContentValues values = new ContentValues(10);
+        values.put(MediaStore.MediaColumns.TITLE, "Recorded");
+        values.put(MediaStore.Audio.Media.ALBUM, "Audio_Album");
+        values.put(MediaStore.Audio.Media.ARTIST, "Ton");
+        values.put(MediaStore.Audio.Media.DISPLAY_NAME, "Recorded Audio");
+        values.put(MediaStore.Audio.Media.IS_RINGTONE, 1);
+        values.put(MediaStore.Audio.Media.IS_MUSIC, 1);
+        values.put(MediaStore.MediaColumns.DATE_ADDED, System.currentTimeMillis()/1000);
+        values.put(MediaStore.MediaColumns.MIME_TYPE, "audio/mp4"); // 미디어 파일의 포맷
+        values.put(MediaStore.Audio.Media.DATA, fileName); // 저장된 녹음 파일
+
+        // ContentValues 객체를 추가할 때, 음성 파일에 대한 내용 제공자 URI 사용
+        return getContentResolver().insert(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, values);
+    }
+
+    /*
+    public void permissionCheck(){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
+                || ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE,Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.RECORD_AUDIO}, 1);        }
+
+    }
+*/
+    public void permissionCheck(){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
+                || ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.RECORD_AUDIO}, 1);        }
+
+    }
     public void SetProgressView(){
         //현재 단계를 나타내는 부분 처리
         StageIndex.setText(Integer.toString(Stagenum) + "단원의 문제를 출력하는 부분입니다.\n현재 문항 : " + Integer.toString(Currentstate) + "번");
